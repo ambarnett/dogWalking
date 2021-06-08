4a7a3d348c394026d524f50959514cb2d122ed4e
ambarnett <andy.barnett09@gmail.com>
2021-06-08 13:34:14 -0500

    added second event listener to see which dog is being walked and where

--- scripts/RegisteredPets.js
+++ scripts/RegisteredPets.js
@@ -1,12 +1,26 @@
 import { getPets } from "./database.js"
+import { getWalkers } from "./database.js"
 
-const pets = getPets()
 
 export const RegisteredPets = () => {
+    
+    const pets = getPets()
+    const walkers = getWalkers()
+
+    document.addEventListener("click",
+    (event) => {
+        if (event.target.id.startsWith("pet")) {
+            const[, petId] = event.target.id.split("--")
+            const clickedPetObj = pets.find( (pet) => pet.id === parseInt(petId))
+            const foundWalker = walkers.find( (walker) => walker.id === clickedPetObj.walkerId)
+            window.alert(`${clickedPetObj.name} is being walked by ${foundWalker.name}`)
+        }
+    })
+
     let petHTML = "<ul>"
 
     for (const pet of pets) {
-        petHTML += `<li>${pet.name}</li>`
+        petHTML += `<li id="pet--${pet.id}">${pet.name}</li>`
     }
 
     petHTML += "</ul>"
